a9ec75ea88940642f2c8fabc534075a745807ca0
yone1130 <admin@yoneyo.com>
2023-12-06 14:00:01 +0900

    [temp] Change directory name

---
+++ .scripts/main.js
@@ -0,0 +1,43 @@
+/*
+ *
+ * main.js | YDITS for Web
+ *
+ * (c) よね/Yone
+ *
+ * No modification or reproduction of any kind is permitted.
+ * 改変や複製を一切禁じます。
+ *
+ */
+
+const version = "3.6.0";
+
+const initPage = () => {
+    initCommonElements();
+}
+
+const initCommonElements = () => {
+    $("header").load("./elements/header.html");
+    $("footer").load("./elements/footer.html");
+}
+
+function win(winId, winTitle) {
+    $('body').append(`
+        <dialog class="dialog" id=${winId}>
+            <div class="navBar">
+                <p class="title"></p>
+                <span class="close material-symbols-outlined">close</span>
+            </div>
+        
+            <div class="content">
+            </div>
+        </dialog>
+    `);
+
+    $(`#${winId}>.navBar>.title`).text(winTitle);
+
+    $(document).on('click', `#${winId}>.navBar>.close`, (event) => {
+        $(`#${winId}`).remove()
+    })
+};
+
+document.addEventListener('DOMContentLoaded', initPage);
