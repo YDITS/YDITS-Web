7f0a4d1a292783b240c2e0e953352ff2946404df
yone1130 <git@yoneyo.com>
2024-07-26 22:01:59 +0900

    Fix: バージョン表記更新忘れ修正

--- src/pages/scripts/ydits-web/ydits-web.mjs
+++ src/pages/scripts/ydits-web/ydits-web.mjs
@@ -36,7 +36,7 @@ export class YditsWeb extends FirebaseApp {
         super({
             name: "YDITS for Web",
             description: "『YDITS for Web』は、地震速報をすぐに確認できるWebアプリケーションです。",
-            version: "3.12.0",
+            version: "3.13.0",
             author: "よね/Yone",
             copyright: "Copyright © よね/Yone",
             firebase: {
