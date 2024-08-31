a9182217daa74027d5bb634b7236933048f469dd
よね/Yone <git@yoneyo.com>
2024-08-31 18:55:52 +0900

    Release: Version 3.15.0

--- src/pages/scripts/ydits-web/ydits-web.mjs
+++ src/pages/scripts/ydits-web/ydits-web.mjs
@@ -40,7 +40,7 @@ export class YditsWeb extends FirebaseApp {
         super({
             name: "YDITS for Web",
             description: "『YDITS for Web』は、防災情報をすぐに確認できるWebアプリケーションです。",
-            version: "3.14.2",
+            version: "3.15.0",
             author: "よね/Yone",
             copyright: "Copyright © よね/Yone",
             firebase: {
