50399e3f19d3d5d2b0c641ab29e2513b2812118b
よね/Yone <git@yoneyo.com>
2024-12-04 02:44:18 +0900

    Change(dmdata): ローディング表示の変更

--- src/pages/scripts/ydits-web/services/api/dmdata.mjs
+++ src/pages/scripts/ydits-web/services/api/dmdata.mjs
@@ -35,7 +35,7 @@ export class Dmdata extends Service {
      * @returns 
      */
     async initialize() {
-        $("#eewTitle").text(`${this.name}をイニシャライズしています…`);
+        this.app.notify.show("message", "", `${this.name}をイニシャライズしています…`);
 
         let settings = this.app.services.settings;
 
