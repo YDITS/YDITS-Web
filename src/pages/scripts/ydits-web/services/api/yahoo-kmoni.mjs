6197fe95936954b767809dcb5f2590767aefcc53
よね/Yone <git@yoneyo.com>
2024-12-06 19:42:15 +0900

    update(yahoo-kmoni): Wolfx への一部機能移行

--- src/pages/scripts/ydits-web/services/api/yahoo-kmoni.mjs
+++ src/pages/scripts/ydits-web/services/api/yahoo-kmoni.mjs
@@ -202,8 +202,8 @@ export class YahooKmoni extends Service {
                                 this.app.services.eew.reports[DATA.reportId].psWave.sRadius = data.psWave.items[0].sRadius;
 
                                 // this.app.services.eew.updateField();
-                                this.app.services.eew.sound();
-                                this.app.services.eew.push();
+                                // this.app.services.eew.sound();
+                                // this.app.services.eew.push();
                             } catch (error) {
                                 console.error(error);
                             }
