aac460b3bf0341a034f8fe8e680bd179f5767b27
よね/Yone <git@yoneyo.com>
2024-12-09 13:34:05 +0900

    fix(app)

--- src/pages/scripts/app.mjs
+++ src/pages/scripts/app.mjs
@@ -29,12 +29,8 @@ export class App {
     /**
      * 新規のサービスを登録します。
      */
-    register(Service) {
-        if (!(Service instanceof Service)) {
-            throw new TypeError('引数はServiceクラスである必要があります');
-        }
-
-        const newService = new Service(this);
+    register(NewService) {
+        const newService = new NewService(this);
 
         if (!newService.name) {
             throw new Error('Serviceにnameプロパティが設定されていません');
