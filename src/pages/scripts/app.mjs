2227799a5ca0f26be1ab0e22dc48ee637f4c4289
よね/Yone <git@yoneyo.com>
2024-12-09 13:18:02 +0900

    refactor(app): add error handling

--- src/pages/scripts/app.mjs
+++ src/pages/scripts/app.mjs
@@ -30,7 +30,16 @@ export class App {
      * 新規のサービスを登録します。
      */
     register(Service) {
+        if (!(Service instanceof Service)) {
+            throw new TypeError('引数はServiceクラスである必要があります');
+        }
+
         const newService = new Service(this);
+
+        if (!newService.name) {
+            throw new Error('Serviceにnameプロパティが設定されていません');
+        }
+
         this.services[newService.name] = newService;
     }
 }
