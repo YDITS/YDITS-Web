18dc8a522774432f6a701f65c89e933ebab62c14
よね/Yone <git@yoneyo.com>
2024-12-22 21:38:08 +0900

    update

--- src/pages/scripts/ydits-web/ydits-web.mjs
+++ src/pages/scripts/ydits-web/ydits-web.mjs
@@ -347,6 +347,18 @@ export class YditsWeb extends FirebaseApp {
         trying(() => {
             document.getElementById("debugOutputP2pquakeSocket").textContent = `P2P地震情報 WebSocket: ${this.services.api.p2pquake.socket ? "Connected" : "Disconnected"}`;
         });
+        
+        trying(() => {
+            let reports = "";
+            Object.keys(this.services.eew.reports[this.services.eew.currentId]).forEach(key => {
+                reports += `${key}: ${this.services.eew.reports[this.services.eew.currentId][key]}, `;
+            });
+            document.getElementById("debugOutputEewData").textContent = `Current EEW Data: ${reports}`;
+        });
+
+        trying(() => {
+            document.getElementById("debugOutputWolfxJmaEewData").textContent = `Wolfx Jma EEW Data: ${JSON.stringify(this.services.api.wolfx.jmaEewData)}`;
+        });
     }
 
 
