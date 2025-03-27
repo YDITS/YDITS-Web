d21bbbf5e3d601d93e821784cfe322286c79ee58
よね/Yone <git@yoneyo.com>
2025-03-28 01:37:51 +0900

    feat(LocalStorage): implement LocalStorage service for managing local storage operations

---
+++ src/pages/scripts/ydits-web/services/modules/local-storage.mjs
@@ -0,0 +1,57 @@
+/*
+ *
+ * YDITS for Web
+ *
+ * Copyright (C) よね/Yone
+ *
+ * Licensed under the Apache License 2.0.
+ *
+ */
+
+import { Service } from "../../../service.mjs";
+
+class LocalStorage extends Service {
+    constructor(app) {
+        super(app, {
+            name: "localStorage",
+            description: "ローカルストレージを扱うサービスです。",
+            version: "0.0.0",
+            author: "よね/Yone",
+            copyright: "Copyright © よね/Yone"
+        });
+
+        this._cacheLocationArea = this._localStorageGetItem(LocalStorage.cacheLocationAreaKey);
+    }
+
+
+    static cacheLocationAreaKey = "cacheLocationArea";
+
+
+    get cacheLocationArea() {
+        return this._cacheLocationArea;
+    }
+
+
+    set cacheLocationArea(value) {
+        this._cacheLocationArea = value;
+        this._localStorageSetItem(LocalStorage.cacheLocationAreaKey, value);
+    }
+
+
+    _localStorageGetItem(key) {
+        try {
+            window.localStorage.getItem(key);
+        } catch (error) {
+            throw new Error(`Could not get localStorage item as ${key}: ${error.stack}`);
+        }
+    }
+
+
+    _localStorageSetItem(key, value) {
+        try {
+            window.localStorage.setItem(key, value);
+        } catch (error) {
+            throw new Error(`Could not set localStorage item as ${key}: ${error.stack}`);
+        }
+    }
+}
