dcdf5fdc179a6076beaf6d65747befef7c201a68
よね/Yone <git@yoneyo.com>
2025-04-19 02:17:53 +0900

    Feat: Switch Common Elements to use the Render library for loading

--- src/pages/scripts/common.mjs
+++ src/pages/scripts/common.mjs
@@ -8,19 +8,62 @@
  *
  */
 
-document.addEventListener("DOMContentLoaded", async () => await loadCommonElements());
+import { Render } from "https://cdn.yoneyo.com/scripts/render/render-v1.0.0.mjs";
 
 
-/**
- * 共通の要素を読み込む
- * @returns {Promise<void>}
- */
-async function loadCommonElements() {
-    await safeCall(fetchAndSetElement, "header", "/elements/header.html");
+document.addEventListener("DOMContentLoaded", async () => {
+    /**
+     * @type {Render}
+     */
+    const render = new Render();
 
-    if (new Set(["/", "/eqhistory/", "/debug-logs/"]).has(location.pathname)) { return; }
-    await safeCall(fetchAndSetElement, "footer", "/elements/footer.html");
-}
+    render.build({
+        target: document.querySelector("header"),
+        children: header(),
+    });
+
+    if (["/", "/eqhistory/", "/debug-logs/"].includes(location.pathname)) return;
+
+    render.build({
+        target: document.querySelector("footer"),
+        children: footer(),
+    });
+
+    /**
+     * @returns {Element[]}
+     */
+    function header() {
+        return [
+            render.$div({
+                className: "wrapper",
+                children: [
+                    render.$h1({
+                        id: "headerTitle",
+                        className: "header__title",
+                        textContent: "YDITS for Web",
+                    }),
+                ],
+            }),
+        ]
+    }
+
+    /**
+     * @returns {Element[]}
+     */
+    function footer() {
+        return [
+            render.$div({
+                className: "wrapper",
+                children: [
+                    render.$p({
+                        id: "footerCopyright",
+                        innerHTML: "&copy; よね/Yone",
+                    }),
+                ],
+            }),
+        ]
+    }
+});
 
 
 /**
@@ -35,36 +78,4 @@ async function safeCall(func, ...args) {
     } catch (error) {
         console.error(error.stack);
     };
-}
-
-
-/**
- * 要素を Fetch して読み込む
- * @param {string} query 
- * @param {string} uri 
- * @returns {Promise<void>}
- */
-async function fetchAndSetElement(query, uri) {
-    const response = await fetch(uri)
-        .catch((error) => {
-            throw new Error(`Failed to fetch html {url: ${uri}}: ${error.message}`, { cause: error });
-        });
-
-    const html = await response.text()
-        .catch((error) => {
-            throw new Error(`Failed to read html {url: ${uri}}: ${error.message}`, { cause: error });
-        });
-
-    let element;
-    try {
-        element = document.querySelector(query);
-    } catch (error) {
-        throw new Error(`Failed to get element {query: ${query}}: ${error.message}`, { cause: error });
-    }
-
-    if (!element) {
-        throw new Error(`Invalid query specified {query: ${query}}: ${error.message}`, { cause: error });
-    }
-
-    element.innerHTML = html;
 }
