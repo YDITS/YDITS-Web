4420749b530dd867453671a7f3d8b4c351d0c3e6
よね/Yone <git@yoneyo.com>
2025-05-03 14:54:46 +0900

    Fix: Update import statement to include .js extension for app module

--- src/pages/scripts/packages/firebase-app-creator/src/app.js
+++ src/pages/scripts/packages/firebase-app-creator/src/app.js
@@ -8,7 +8,7 @@
  *
  */
 
-import { App } from "../../app-creator/src/app";
+import { App } from "../../app-creator/src/app.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
 
